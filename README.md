0522472e1932e0ac829bccd71e167dcaf77055ac
Pablo <pablopissi@gmail.com>
2020-07-04 01:23:50 -0300

    Organizo serial.js, agrego comando a package.json y README

---
+++ README.md
@@ -0,0 +1,9 @@
+# ArduIndoor
+
+Un dashboard hecho en React.js, Node.js para controlar tus plantas utilizando Arduino.
+
+### Comandos
+Para iniciar la conexión vía puerto serie
+```
+npm run serial
+```
--- serial.js
+++ serial.js
@@ -1,40 +1,34 @@
 const SerialPort = require('serialport');
 const mqtt = require('mqtt')
 const fs = require('fs')
+const logger = require('simple-node-logger')
 
 
-const data = fs.readFileSync('./config.json')
+const configContent = fs.readFileSync('./config.json')
+const { MQTT_CONNECTION, SERIAL_PORT, LOG_CONFIG } = JSON.parse(configContent)
+const log = logger.createSimpleFileLogger(LOG_CONFIG);
 
-	// if (err){
-	// 	log.error('Cannot read configuration from config.json')
-	// 	throw err
-	// }
+log.info(`Starting serial port bridge.`)
 
-const { MQTT_CONNECTION, SERIAL_PORT, LOG_CONFIG } = JSON.parse(data)
-const log = require('simple-node-logger').createSimpleFileLogger(LOG_CONFIG);
-
-log.info(`Starting serial port bridge`)
-
-log.info(`Reading configuration from config.json`)
-const checkAvailable = portName => SerialPort.list().then(ports => ports.some(port => port.path === portName ));
+function checkAvailable(portName) {
+	return SerialPort.list().then(ports => ports.some(port => port.path === portName))
+} 
 
 checkAvailable(SERIAL_PORT).then(status => {
 	if (!status){
-		throw new Error(`Serial port at ${SERIAL_PORT} is not available`)
+		throw new Error(`Serial port at ${SERIAL_PORT} is not available.`)
 	}
 	else{
 		const serialPort = new SerialPort(SERIAL_PORT)
 		const client = mqtt.connect(MQTT_CONNECTION)
+		log.info(`Serial port at ${SERIAL_PORT} is connected.`)
 
 		client.on('connect', () => {
+			log.info(`MQTT client connected at ${MQTT_CONNECTION}.`)
+
 			serialPort.on('data', (data) => {
-				console.log(`Received data through serial port`)
 				client.publish('home/casa', 'Hello mqtt')
 			})
-			log.info(`MQTT client connected at ${MQTT_CONNECTION}`)
 		})
 	}
-}).catch(e => {
-	log.error(`Serial port at ${SERIAL_PORT} is not available`)
-})
-
+}).catch(e => log.error(e.message))
