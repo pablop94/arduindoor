82e8ca5c23f992ada52bf6bb770eb1238b0b1ac4
Pablo <pablopissi@gmail.com>
2020-07-16 22:27:53 -0300

    Agrego notificacion a telegram

--- configuration.js
+++ configuration.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
 const configContent = fs.readFileSync('./config.json')
+require('dotenv').config()
 
 module.exports = JSON.parse(configContent)
--- notifications.js
+++ notifications.js
@@ -1,18 +1,28 @@
 const mqtt = require('mqtt')
 const { MQTT_CONNECTION } = require('./configuration')
 const get_logger = require('./logger')
+const axios = require('axios')
 
 const log = get_logger({ category: "NOTIFICATIONS" });
 
 log.info(`Starting notifications listener.`)
 
 const client = mqtt.connect(MQTT_CONNECTION)
-
+let sent = false
 client.on('connect', () => {
   log.info(`MQTT client connected at ${MQTT_CONNECTION}.`)
   client.subscribe('temperatura', function (err, grants) {
     grants.forEach(grant => log.info(`Successfully suscribed notifications to topic: ${grant.topic}`))
   })
   client.on('message', function (topic, message) {
+    const send_text = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${process.env.TELEGRAM_CHAT_ID}&parse_mode=Markdown&text=${message.toString()}`
+    if (!sent) {
+      sent = true
+      axios.get(send_text).then(response => {
+        log.info(`Message sent: ${message.toString()}`)
+      }).catch((err) => {
+        log.error(err)
+      })
+    }
   })
 })
