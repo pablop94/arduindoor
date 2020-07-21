6740a765a9e208deef712229d4c914c9ef4d36e0
florencia <florencia@camba.coop>
2020-07-21 17:34:34 -0300

    Se inicializa frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,298 @@
+import React from 'react';
+import './App.css';
+import TemperatureGraph from './components/TemperatureChart';
+import Temperature from './components/Temperature';
+
+function App() {
+
+  const datita = [
+    {
+      "id": "japan",
+      "color": "hsl(47, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 281
+        },
+        {
+          "x": "helicopter",
+          "y": 52
+        },
+        {
+          "x": "boat",
+          "y": 40
+        },
+        {
+          "x": "train",
+          "y": 211
+        },
+        {
+          "x": "subway",
+          "y": 199
+        },
+        {
+          "x": "bus",
+          "y": 261
+        },
+        {
+          "x": "car",
+          "y": 280
+        },
+        {
+          "x": "moto",
+          "y": 142
+        },
+        {
+          "x": "bicycle",
+          "y": 67
+        },
+        {
+          "x": "horse",
+          "y": 255
+        },
+        {
+          "x": "skateboard",
+          "y": 248
+        },
+        {
+          "x": "others",
+          "y": 285
+        }
+      ]
+    },
+    {
+      "id": "france",
+      "color": "hsl(202, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 166
+        },
+        {
+          "x": "helicopter",
+          "y": 180
+        },
+        {
+          "x": "boat",
+          "y": 42
+        },
+        {
+          "x": "train",
+          "y": 177
+        },
+        {
+          "x": "subway",
+          "y": 199
+        },
+        {
+          "x": "bus",
+          "y": 136
+        },
+        {
+          "x": "car",
+          "y": 270
+        },
+        {
+          "x": "moto",
+          "y": 292
+        },
+        {
+          "x": "bicycle",
+          "y": 145
+        },
+        {
+          "x": "horse",
+          "y": 102
+        },
+        {
+          "x": "skateboard",
+          "y": 10
+        },
+        {
+          "x": "others",
+          "y": 246
+        }
+      ]
+    },
+    {
+      "id": "us",
+      "color": "hsl(130, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 253
+        },
+        {
+          "x": "helicopter",
+          "y": 116
+        },
+        {
+          "x": "boat",
+          "y": 51
+        },
+        {
+          "x": "train",
+          "y": 29
+        },
+        {
+          "x": "subway",
+          "y": 12
+        },
+        {
+          "x": "bus",
+          "y": 38
+        },
+        {
+          "x": "car",
+          "y": 285
+        },
+        {
+          "x": "moto",
+          "y": 294
+        },
+        {
+          "x": "bicycle",
+          "y": 32
+        },
+        {
+          "x": "horse",
+          "y": 38
+        },
+        {
+          "x": "skateboard",
+          "y": 42
+        },
+        {
+          "x": "others",
+          "y": 276
+        }
+      ]
+    },
+    {
+      "id": "germany",
+      "color": "hsl(183, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 289
+        },
+        {
+          "x": "helicopter",
+          "y": 248
+        },
+        {
+          "x": "boat",
+          "y": 10
+        },
+        {
+          "x": "train",
+          "y": 214
+        },
+        {
+          "x": "subway",
+          "y": 20
+        },
+        {
+          "x": "bus",
+          "y": 126
+        },
+        {
+          "x": "car",
+          "y": 55
+        },
+        {
+          "x": "moto",
+          "y": 296
+        },
+        {
+          "x": "bicycle",
+          "y": 224
+        },
+        {
+          "x": "horse",
+          "y": 63
+        },
+        {
+          "x": "skateboard",
+          "y": 99
+        },
+        {
+          "x": "others",
+          "y": 133
+        }
+      ]
+    },
+    {
+      "id": "norway",
+      "color": "hsl(332, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 292
+        },
+        {
+          "x": "helicopter",
+          "y": 285
+        },
+        {
+          "x": "boat",
+          "y": 191
+        },
+        {
+          "x": "train",
+          "y": 115
+        },
+        {
+          "x": "subway",
+          "y": 38
+        },
+        {
+          "x": "bus",
+          "y": 268
+        },
+        {
+          "x": "car",
+          "y": 283
+        },
+        {
+          "x": "moto",
+          "y": 146
+        },
+        {
+          "x": "bicycle",
+          "y": 222
+        },
+        {
+          "x": "horse",
+          "y": 23
+        },
+        {
+          "x": "skateboard",
+          "y": 122
+        },
+        {
+          "x": "others",
+          "y": 75
+        }
+      ]
+    }
+  ]
+
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h1>Arduindoor</h1>
+      </div>
+      <div className="data-container">
+        <div className="data-card">
+          <Temperature />
+        </div>
+      </div>
+      <div className="chart">
+        <TemperatureGraph data={datita} />
+      </div>
+		</div>
+  );
+}
+
+export default App
---
+++ frontend/src/components/Temperature.jsx
@@ -0,0 +1,104 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import WavesTwoToneIcon from '@material-ui/icons/WavesTwoTone';
+import OpacityTwoToneIcon from '@material-ui/icons/OpacityTwoTone';
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: '#e3e8e4',
+    borderRightColor: '#5ad65a',
+    borderRightWidth: '30px',
+    minWidth: '25vw',
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 18,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  dataContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  }
+});
+
+const mqtt = require('mqtt');
+
+export default function Temperature(props) {
+  const classes = useStyles();
+
+  const options = {
+    protocol: 'websockets'
+  };
+  const client  = mqtt.connect('mqtt://0.0.0.0:9001', options);
+
+  // Sets default React state 
+  const [humidity, setHumidity] = useState(<Fragment><em>nothing heard</em></Fragment>);
+  const [temerature, setTemperature] = useState(<Fragment><em>nothing heard</em></Fragment>);
+
+  useEffect(() => {
+    // temperatura is a the MQTT topic
+    client.subscribe('temperatura');  
+
+    // humedad is a the MQTT topic
+    client.subscribe('humedad');  
+    client.on('message', function (topic, message) {
+      console.log('topic ', topic)
+      console.log('valor ', message.toString())
+       // Updates React state with message 
+      topic === 'temperatura' ?  setTemperature(message.toString()) :  setHumidity(message.toString())
+     
+      // client.end();
+    });       
+  }, []);          
+ 
+
+  return(
+    <Box className={classes.dataContainer}>
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography className={classes.title} color="textSecondary" gutterBottom>
+            Temperatura
+          </Typography>
+          <WavesTwoToneIcon />
+          <Typography className={classes.pos} color="textSecondary">
+            {temerature}
+          </Typography>
+          <Typography variant="body2" component="p">
+            Temperatura óptima
+            <br />
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography className={classes.title} color="textSecondary" gutterBottom>
+            Humedad
+          </Typography>
+          <OpacityTwoToneIcon />
+          <Typography className={classes.pos} color="textSecondary">
+            {humidity}
+          </Typography>
+          <Typography variant="body2" component="p">
+            Se recomienda regar pronto
+            <br />
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
+  )
+  
+  
+}
+
